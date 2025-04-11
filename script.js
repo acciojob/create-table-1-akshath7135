e71e191933edc79cc73ed8da2a6e3ef76f12ebf5
akshath7135 <akshathabrol21122004@gmail.com>
2025-04-11 18:39:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function insert_Row() {
-	let table = document.getElementById("sampleTable");
-	let row = table.insertRow(0);
-	let newrow= row.insertCell(0);
-	let newrow1 = row.insertCell(1);
-	newrow.textContent="New Cell1";
-	newrow1.textContent="New Cell2";
-  
-  
+    let table = document.getElementById("sampleTable");
+    let row = table.insertRow(0); // Insert a new row at the top
+    let newCell1 = row.insertCell(0); // Insert a new cell in the row
+    let newCell2 = row.insertCell(1); // Insert another new cell in the row
+    newCell1.textContent = "New Cell1"; // Set the text content of the first cell
+    newCell2.textContent = "New Cell2"; // Set the text content of the second cell
 }
-button.addEventlistener("onclick",insert_Row);
